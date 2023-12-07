37568ae130945402adb89a520ec2344f30e5c4fb
Arda <94324501+Ardacanuysal@users.noreply.github.com>
2023-12-07 17:38:18 +0300

    a
    A

--- Arda/ViewController.swift
+++ Arda/ViewController.swift
@@ -1,19 +1,79 @@
-//
-//  ViewController.swift
-//  Arda
-//
-//  Created by Arda Can UYSAL on 1.12.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+
+    let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    func setupUI() {
+        view.backgroundColor = .white
 
+        // Stack View
+        let stackView = UIStackView(arrangedSubviews: [usernameTextField, passwordTextField, loginButton])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(stackView)
+
+        // Constraints
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+
+    @objc func loginButtonTapped() {
+        // Giriş işlemlerini burada gerçekleştirin
+        if let username = usernameTextField.text, let password = passwordTextField.text {
+            print("Username: \(username), Password: \(password)")
+            // Gerekli doğrulama veya diğer işlemleri ekleyin
+        }
+    }
 }
+
+// Ekran önizleme için kullanabilirsiniz
+#if DEBUG
+import SwiftUI
+
+struct LoginViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        Container().edgesIgnoringSafeArea(.all)
+    }
+
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            return LoginViewController()
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    }
+}
+#endif
 
---
+++ deneme/deneme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  deneme
+//
+//  Created by Arda Can UYSAL on 7.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
