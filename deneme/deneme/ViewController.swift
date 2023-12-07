397f56fa0eb2d9c633265f734b685bee495ced95
Arda <94324501+Ardacanuysal@users.noreply.github.com>
2023-12-07 17:49:31 +0300

    firstcommit
    ardacem

--- deneme/deneme/ViewController.swift
+++ deneme/deneme/ViewController.swift
@@ -1,19 +1,79 @@
-//
-//  ViewController.swift
-//  deneme
-//
-//  Created by Arda Can UYSAL on 7.12.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+
+    // MARK: - UI Components
+
+    let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+
+        setupUI()
     }
 
+    // MARK: - UI Setup
 
+    private func setupUI() {
+        view.addSubview(usernameTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            passwordTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 30),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
+    // MARK: - Actions
+
+    @objc private func loginButtonTapped() {
+        // Implement your authentication logic here
+        // For simplicity, let's just print the entered username and password
+        if let username = usernameTextField.text, let password = passwordTextField.text {
+            print("Username: \(username), Password: \(password)")
+        }
+    }
 }
 
+// MARK: - Playground Usage
+
+import PlaygroundSupport
+
+let loginViewController = LoginViewController()
+PlaygroundPage.current.liveView = loginViewController
